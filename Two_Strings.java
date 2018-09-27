8890c9e49e6c4672195ab3771535488bb9df72f6
DaLisa Denham <dalisadenham@DaLisas-MacBook-Pro.local>
2018-09-26 23:46:55 -0400

    hello

---
+++ Two_Strings.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        int n=s1.length();
+        int x=s2.length();
+        String answer="NO";
+        for (int i=0; i<n; i++) {
+
+            for(int j=0; j<x; j++) {
+                if (s1.charAt(i)==s2.charAt(j)) {
+                   answer= "YES";
+                } 
+            
+            }
+        }
+        return answer;
+    } 
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
